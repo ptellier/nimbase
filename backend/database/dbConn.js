eee5e195d6f8e5077e5611caf494588a228d371c
Aryan-B <baryan.edu@gmail.com>
2023-05-30 19:02:13 -0700

    db query successful

---
+++ backend/database/dbConn.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv");
+const uri = dotenv.config().parsed.MONGODB_URI;
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let conn;
+async function run() {
+    try {
+        conn = await client.connect();
+        console.log("Connected successfully to server");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+const db = client.db("stormhacks");
+module.exports = db
--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,17 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const dotenv = require('dotenv');
-
-const uri = dotenv.config().parsed.MONGODB_URI;
-// console.log(uri)
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-//create a express server at port 8080
+const db  = require('./database/dbConn.js');
 const express = require('express');
+const port = 8080;
 const app = express();
 
-const port = 8080;
-
 app.get('/', (req, res) => {
-    run()
     res.send('Hello World!');
     }
 );
 
-app.get('/api', (req, res) => {
-    res.send('Hello World! from api');
+app.get('/api', async (req, res) => {
+    const collection = db.collection("context");
+    const result = await collection.find({}).toArray();
+    res.send(result);
     }
 );
 
@@ -51,8 +19,3 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
     }
 );
-
-
-
-//write a router
-// const router = express.Router()
