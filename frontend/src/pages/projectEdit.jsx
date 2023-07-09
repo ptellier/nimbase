aac5b9e795877fb504680e58d9c012317c80d540
Phillip Tellier <PhillipFTellier@gmail.com>
2023-07-09 12:17:44 -0700

    Styled projectEdit page, added form validation, added image upload, and added new fields (entry port, environment variables)

--- frontend/src/pages/projectEdit.jsx
+++ frontend/src/pages/projectEdit.jsx
@@ -7,6 +7,8 @@ import {accessTokenSelector, usernameSelector} from "../state/userSlice";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faWandMagicSparkles} from "@fortawesome/free-solid-svg-icons";
 import Query from "../components/Query";
+import FieldArea from "../components/FieldArea";
+import ImageUploader from "../components/ImageUploader";
 
 const FIELD_WIDTH = "20rem";
 const query = new Query();
@@ -15,6 +17,7 @@ const ProjectEdit = () => {
   const [submitAttempted, setSubmitAttempted] = useState(false);
   const [projectNameError, setProjectNameError] = useState(false);
   const [projectDescError, setProjectDescError] = useState(false);
+  const [projectImageError, setProjectImageError] = useState(false);
   const [dockerfileError, setDockerfileError] = useState(false);
   const [githubLinkError, setGithubLinkError] = useState(false);
   const [githubAuthTokenError, setGithubAuthTokenError] = useState(false);
@@ -28,17 +31,41 @@ const ProjectEdit = () => {
     owner: username,
     name: "",
     description: "",
+    image: null,
     public: true,
     dockerfile: "",
     github_url: "",
     github_auth_tokens: "",
+    env_vars: "",
+    entry_port: "",
   });
+
+  const setImage = (value) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      image: value,
+    }));
+  }
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
+    }));
+  }
+
+  const handleInputChangeParseInt = (e) => {
+    let value;
+    try {
+      value = (e.target.value === "") ? "" : parseInt(e.target.value);
+    } catch (error) {
+      console.error("Error parsing int when handling form input");
+      return;
+    }
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [e.target.name]: value,
     }));
   }
 
@@ -69,9 +96,16 @@ const ProjectEdit = () => {
     let valid = true;
     if (formData.name === "") {setProjectNameError("Cannot be blank"); valid = false;} else {setProjectNameError(false);}
     if (formData.description === "") {setProjectDescError("Cannot be blank"); valid = false;} else {setProjectDescError(false);}
+    if (formData.image === null) {setProjectImageError("Must select an image"); valid = false;} else {setProjectImageError(false);}
     if (formData.dockerfile === "") {setDockerfileError("Cannot be blank"); valid = false;} else {setDockerfileError(false);}
     if (formData.github_url === "") {setGithubLinkError("Cannot be blank"); valid = false;} else {setGithubLinkError(false);}
     if (formData.github_auth_tokens === "") {setGithubAuthTokenError("Cannot be blank"); valid = false;} else {setGithubAuthTokenError(false);}
+    if (typeof formData.entry_port !== "number" || formData.entry_port < 1 || formData.entry_port > 65536) {
+      setEntryPortError("Must be a number (1-65536)");
+      valid = false;
+    } else {
+      setEntryPortError(false);
+    }
     return valid;
   }
 
@@ -89,19 +123,27 @@ const ProjectEdit = () => {
         <div className="project-edit-container">
           <Field label="Project Name" type="text" name="name" value={formData.name}
                  onChange={handleInputChange} error={projectNameError} width={FIELD_WIDTH}/>
-          <Field label="Project Description" type="text" name="description" value={formData.description}
-                 onChange={handleInputChange} error={projectDescError} width={FIELD_WIDTH}/>
+          <div className="image-and-desc-container">
+            <ImageUploader image={formData.image} setImage={setImage} error={projectImageError}/>
+            <FieldArea label="Project Description" name="description" value={formData.description}
+                  onChange={handleInputChange} error={projectDescError} width={FIELD_WIDTH} cols={45} rows={10}/>
+          </div>
           <Field label="Dockerfile" type="text" name="dockerfile" value={formData.dockerfile}
                  onChange={handleInputChange} error={dockerfileError} width={FIELD_WIDTH}/>
-          <Field label="Github Link" type="text" name="github_url" value={formData.github_url}
-                  onChange={handleInputChange} error={githubLinkError} width={FIELD_WIDTH}/>
-          <Field label="Github Auth Token" type="text" name="github_auth_tokens" value={formData.github_auth_tokens}
-                  onChange={handleInputChange} error={githubAuthTokenError} width={FIELD_WIDTH}/>
-          {/*<Field label="Environment Variables" type="text" name="env_vars" value={formData.env_vars}*/}
-          {/*        onChange={handleInputChange} error={envVarError}/>*/}
+          <div className="github-fields-container">
+            <Field label="Github Link" type="text" name="github_url" value={formData.github_url}
+                    onChange={handleInputChange} error={githubLinkError} width={FIELD_WIDTH}/>
+            <Field label="Github Auth Token" type="text" name="github_auth_tokens" value={formData.github_auth_tokens}
+                    onChange={handleInputChange} error={githubAuthTokenError} width={FIELD_WIDTH}/>
+          </div>
+          <Field label="Entry Port" type="number" name="entry_port" value={formData.entry_port}
+                  onChange={handleInputChangeParseInt} error={entryPortError} width={FIELD_WIDTH}/>
+          <FieldArea label="Environment Variables" name="env_vars" value={formData.env_vars}
+                  onChange={handleInputChange} error={envVarError} cols={45} rows={4}/>
           <div className="submit-button-container">
             <button>Submit <FontAwesomeIcon icon={faWandMagicSparkles}/></button>
           </div>
+
         </div>
       </form>
     </div>
