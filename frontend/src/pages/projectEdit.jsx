d5b314cd4beeecc904af33ebc4af24034299a199
landseer <landseerenga@gmail.com>
2023-07-14 17:46:21 -0700

    added help icons

--- frontend/src/pages/projectEdit.jsx
+++ frontend/src/pages/projectEdit.jsx
@@ -5,7 +5,7 @@ import {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
 import {accessTokenSelector, usernameSelector} from "../state/userSlice";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faWandMagicSparkles} from "@fortawesome/free-solid-svg-icons";
+import {faWandMagicSparkles, faQuestionCircle} from "@fortawesome/free-solid-svg-icons";
 import Query from "../components/Query";
 import FieldArea from "../components/FieldArea";
 import ImageUploader from "../components/ImageUploader";
@@ -118,6 +118,13 @@ const ProjectEdit = () => {
     }
   }, [formData]);
 
+  const [showHelpIconDockerfile, setShowHelpIconDockerfile] = useState(false);
+  const [showHelpIconGithub, setShowHelpIconGithub] = useState(false);
+  const [showHelpIconAuth, setShowHelpIconAuth] = useState(false);
+  const [showHelpIconPort, setShowHelpIconPort] = useState(false);
+  const [showHelpIconEnv, setShowHelpIconEnv] = useState(false);
+
+
   return (
     <div className="background-image">
       <NavBar/>
@@ -131,18 +138,80 @@ const ProjectEdit = () => {
             <FieldArea label="Project Description" name="description" value={formData.description}
                   onChange={handleInputChange} error={projectDescError} width={FIELD_WIDTH} cols={45} rows={10}/>
           </div>
+          <div className="image-and-desc-container">
           <Field label="Dockerfile" type="text" name="dockerfile" value={formData.dockerfile}
                  onChange={handleInputChange} error={dockerfileError} width={FIELD_WIDTH}/>
+            <div className="help-icon-container">
+               <span
+                  className={`help-icon ${showHelpIconDockerfile ? 'visible' : ''}`}
+                 onMouseEnter={() => setShowHelpIconDockerfile(true)}
+                  onMouseLeave={() => setShowHelpIconDockerfile(false)}
+                      >
+                       <FontAwesomeIcon icon={faQuestionCircle} />
+                   </span>
+              {showHelpIconDockerfile && (
+                  <div className="help-text">Upload a valid dockerfile for your project</div>
+              )}
+            </div>
+            </div>
           <div className="github-fields-container">
             <Field label="Github Link" type="text" name="github_url" value={formData.github_url}
                     onChange={handleInputChange} error={githubLinkError} width={FIELD_WIDTH}/>
+            <div className="help-icon-container">
+               <span
+                   className={`help-icon ${showHelpIconGithub ? 'visible' : ''}`}
+                   onMouseEnter={() => setShowHelpIconGithub(true)}
+                   onMouseLeave={() => setShowHelpIconGithub(false)}
+               >
+                       <FontAwesomeIcon icon={faQuestionCircle} />
+                   </span>
+              {showHelpIconGithub && (
+                  <div className="help-text">Type a valid GitHuh link</div>
+              )}
+            </div>
             <Field label="Github Auth Token" type="text" name="github_auth_tokens" value={formData.github_auth_tokens}
                     onChange={handleInputChange} error={githubAuthTokenError} width={FIELD_WIDTH}/>
+            <div className="help-icon-container">
+               <span
+                   className={`help-icon ${showHelpIconAuth ? 'visible' : ''}`}
+                   onMouseEnter={() => setShowHelpIconAuth(true)}
+                   onMouseLeave={() => setShowHelpIconAuth(false)}
+               >
+                       <FontAwesomeIcon icon={faQuestionCircle} />
+                   </span>
+              {showHelpIconAuth && (
+                  <div className="help-text">Input a valid GitHub auth token</div>
+              )}
+            </div>
           </div>
           <Field label="Entry Port" type="number" name="entry_port" value={formData.entry_port}
                   onChange={handleInputChangeParseInt} error={entryPortError} width={FIELD_WIDTH}/>
+          <div className="help-icon-container">
+               <span
+                   className={`help-icon ${showHelpIconPort ? 'visible' : ''}`}
+                   onMouseEnter={() => setShowHelpIconPort(true)}
+                   onMouseLeave={() => setShowHelpIconPort(false)}
+               >
+                       <FontAwesomeIcon icon={faQuestionCircle} />
+                   </span>
+            {showHelpIconPort && (
+                <div className="help-text">Input a valid entry port </div>
+            )}
+          </div>
           <FieldArea label="Environment Variables" name="env_vars" value={formData.env_vars}
                   onChange={handleInputChange} error={envVarError} cols={45} rows={4}/>
+          <div className="help-icon-container">
+               <span
+                   className={`help-icon ${showHelpIconEnv ? 'visible' : ''}`}
+                   onMouseEnter={() => setShowHelpIconEnv(true)}
+                   onMouseLeave={() => setShowHelpIconEnv(false)}
+               >
+                       <FontAwesomeIcon icon={faQuestionCircle} />
+                   </span>
+            {showHelpIconEnv && (
+                <div className="help-text">Enter all required environment variables</div>
+            )}
+          </div>
           <div className="submit-button-container">
             <button>Submit <FontAwesomeIcon icon={faWandMagicSparkles}/></button>
           </div>
