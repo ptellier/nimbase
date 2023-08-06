8d839cf557bba8d065e0cf2a3dc781331e4d0958
jakemilad <jake.milad@gmail.com>
2023-08-05 20:34:58 -0700

    trinity test

--- frontend/src/pages/teams.jsx
+++ frontend/src/pages/teams.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import NavBar from "../components/NavBar";
 import { useSelector, useDispatch } from "react-redux";
 import {
--- frontend/src/state/userSlice.js
+++ frontend/src/state/userSlice.js
@@ -28,21 +28,20 @@ export let INITIAL_STATE = {
     //   members: [],
 }
 export const query = new Query();
+
 export const userSlice = createSlice({
     name: "user",
     initialState: INITIAL_STATE,
     extraReducers: (builder) => {
         builder
-            .addCase(logout.fulfilled, (state) => {
-                state.accessToken = "";
-                state.username = undefined;
-                state.email = undefined;
-                state.loginError = undefined;
-                state.signupError = undefined;
-                state.teams = [];
-            })
-            .addCase(fetchUserTeams.fulfilled, (state, action) => {
-                state.teams = action.payload.teams;
+            .addCase(login.fulfilled, (state, action) => {
+                if(action.payload.accessToken){
+                    state.username = action.payload.username;
+                    state.email = action.payload.email;
+                    state.accessToken = action.payload.accessToken;
+                } else {
+                    state.loginError = action.payload.loginError;
+                }
             })
             .addCase(signup.fulfilled, (state, action) => {
                 if(action.payload.accessToken){
@@ -53,47 +52,144 @@ export const userSlice = createSlice({
                     state.signupError = action.payload.signupError;
                 }
             })
+            .addCase(logout.fulfilled, (state) => {
+                return INITIAL_STATE;
+            })
             .addCase(refresh.fulfilled, (state, action) => {
                 state.accessToken = action.payload.accessToken;
             })
+            .addCase(googleLogin.fulfilled, (state, action) => {
+                if(action.payload.accessToken){
+                    state.username = action.payload.username;
+                    state.email = action.payload.email;
+                    state.accessToken = action.payload.accessToken;
+                } else {
+                    state.loginError = action.payload.loginError;
+                }
+            })
+            .addCase(fetchUserTeams.fulfilled, (state, action) => {
+                state.teams = action.payload.teams;
+            })
             .addCase(createTeam.fulfilled, (state, action) => {
-                state.teamName = action.payload.teamName;
-                state.description = action.payload.description;
-                state.owner = action.payload.owner;
-                state.accessToken = action.payload.accessToken;
-            })
-            .addCase(removeTeamMember.fulfilled, (state, action) => {
-                state.accessToken = action.payload.accessToken;
-            })
-            .addCase(addTeamProject.fulfilled, (state, action) => {
-                state.projects = action.payload.projects;
-                state.accessToken = action.payload.accessToken;
-            })
-            .addCase(removeTeamProject.fulfilled, (state, action) => {
-                state.projects = action.payload.projects;
+                state.teams.push({
+                    teamName: action.payload.teamName,
+                    description: action.payload.description,
+                    owner: action.payload.owner,
+                    members: [],
+                    projects: [],
+                });
                 state.accessToken = action.payload.accessToken;
             })
             .addCase(getTeam.fulfilled, (state, action) => {
-                state.teamName = action.payload.teamName;
-                state.description = action.payload.description;
-                state.owner = action.payload.owner;
-                state.projects = action.payload.projects;
+                const team = state.teams.find(t => t.teamName === action.payload.teamName);
+                if (team) {
+                    team.description = action.payload.description;
+                    team.owner = action.payload.owner;
+                    team.projects = action.payload.projects;
+                }
                 state.accessToken = action.payload.accessToken;
             })
-            .addMatcher(isAnyOf(login.fulfilled, logout.fulfilled, signup.fulfilled, googleLogin.fulfilled),
-                (state, action) => {
-                    state.accessToken = action.payload.accessToken;
-                    state.username = action.payload.username;
-                    state.email = action.payload.email;
-                    state.loginError = action.payload.loginError;
-                    state.signupError = action.payload.signupError;
-                })
-            .addMatcher(isAnyOf(login.rejected, logout.rejected, signup.rejected),
+            .addCase(addTeamMember.fulfilled, (state, action) => {
+                const team = state.teams.find(t => t.teamName === action.payload.teamName);
+                if (team) {
+                    team.members.push(action.payload.username);
+                }
+                state.accessToken = action.payload.accessToken;
+            })
+            .addCase(removeTeamMember.fulfilled, (state, action) => {
+                const team = state.teams.find(t => t.teamName === action.payload.teamName);
+                if (team) {
+                    team.members = team.members.filter(m => m !== action.payload.username);
+                }
+                state.accessToken = action.payload.accessToken;
+            })
+            .addCase(addTeamProject.fulfilled, (state, action) => {
+                const team = state.teams.find(t => t.teamName === action.payload.teamName);
+                if (team) {
+                    team.projects.push(action.payload.projectName);
+                }
+                state.accessToken = action.payload.accessToken;
+            })
+            .addCase(removeTeamProject.fulfilled, (state, action) => {
+                const team = state.teams.find(t => t.teamName === action.payload.teamName);
+                if (team) {
+                    team.projects = team.projects.filter(p => p !== action.payload.projectName);
+                }
+                state.accessToken = action.payload.accessToken;
+            })
+            .addMatcher(isAnyOf(login.rejected, logout.rejected, signup.rejected, googleLogin.rejected, refresh.rejected),
                 (state, action) => {
                     console.log("error: thunk rejected; ", action.error.message);
                 });
     }
 })
+
+// export const userSlice = createSlice({
+//     name: "user",
+//     initialState: INITIAL_STATE,
+//     extraReducers: (builder) => {
+//         builder
+//             .addCase(logout.fulfilled, (state) => {
+//                 state.accessToken = "";
+//                 state.username = undefined;
+//                 state.email = undefined;
+//                 state.loginError = undefined;
+//                 state.signupError = undefined;
+//                 state.teams = [];
+//             })
+//             .addCase(fetchUserTeams.fulfilled, (state, action) => {
+//                 state.teams = action.payload.teams;
+//             })
+//             .addCase(signup.fulfilled, (state, action) => {
+//                 if(action.payload.accessToken){
+//                     state.username = action.payload.username;
+//                     state.email = action.payload.email;
+//                     state.accessToken = action.payload.accessToken;
+//                 } else {
+//                     state.signupError = action.payload.signupError;
+//                 }
+//             })
+//             .addCase(refresh.fulfilled, (state, action) => {
+//                 state.accessToken = action.payload.accessToken;
+//             })
+//             .addCase(createTeam.fulfilled, (state, action) => {
+//                 state.teamName = action.payload.teamName;
+//                 state.description = action.payload.description;
+//                 state.owner = action.payload.owner;
+//                 state.accessToken = action.payload.accessToken;
+//             })
+//             .addCase(removeTeamMember.fulfilled, (state, action) => {
+//                 state.accessToken = action.payload.accessToken;
+//             })
+//             .addCase(addTeamProject.fulfilled, (state, action) => {
+//                 state.projects = action.payload.projects;
+//                 state.accessToken = action.payload.accessToken;
+//             })
+//             .addCase(removeTeamProject.fulfilled, (state, action) => {
+//                 state.projects = action.payload.projects;
+//                 state.accessToken = action.payload.accessToken;
+//             })
+//             .addCase(getTeam.fulfilled, (state, action) => {
+//                 state.teamName = action.payload.teamName;
+//                 state.description = action.payload.description;
+//                 state.owner = action.payload.owner;
+//                 state.projects = action.payload.projects;
+//                 state.accessToken = action.payload.accessToken;
+//             })
+//             .addMatcher(isAnyOf(login.fulfilled, logout.fulfilled, signup.fulfilled, googleLogin.fulfilled),
+//                 (state, action) => {
+//                     state.accessToken = action.payload.accessToken;
+//                     state.username = action.payload.username;
+//                     state.email = action.payload.email;
+//                     state.loginError = action.payload.loginError;
+//                     state.signupError = action.payload.signupError;
+//                 })
+//             .addMatcher(isAnyOf(login.rejected, logout.rejected, signup.rejected),
+//                 (state, action) => {
+//                     console.log("error: thunk rejected; ", action.error.message);
+//                 });
+//     }
+// })
 
 export const loginErrorMessageSelector = (state) => state.user.loginError;
 export const signupErrorMessageSelector = (state) => state.user.signupError;
